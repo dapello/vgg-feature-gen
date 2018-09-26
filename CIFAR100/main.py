f7b1aa06e94790b8adb4e9e4a5558b4402c30293
joel <joel@biobright.org>
2018-09-26 00:04:30 -0400

    fix checkpoint label

--- CIFAR100/main.py
+++ CIFAR100/main.py
@@ -212,7 +212,7 @@ def main():
             'epoch': epoch + 1,
             'state_dict': model.state_dict(),
             'best_prec1': best_prec1,
-        }, is_best, filename=os.path.join(args.save_dir, 'checkpoint_{}.tar'.format(epoch)))
+        }, is_best, filename=os.path.join(args.save_dir, 'checkpoint_{}.tar'.format(epoch + 1)))
 
 
 def train(train_loader, model, criterion, optimizer, epoch):
